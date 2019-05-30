9f41fd97408b742de829dd1c2eea51ed9cc17409
anmutu <anmutu@hotmail.com>
2019-05-30 21:43:56 +0800

    评论表设计。

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from course.models import Course
+from datetime import datetime
 
 
-# 用户
 class UserProfile(AbstractUser):
+    """用户"""
     nick_name = models.CharField(max_length=50, verbose_name=u"昵称", default="")
     birthday = models.DateField(verbose_name=u"生日", null=True, blank=True)
-    gender = models.CharField(max_length=6, choices=(("male",u"男"),("female","女")), default="female")
+    gender = models.CharField(max_length=6, choices=(("male", u"男"), ("female", "女")), default="female")
     address = models.CharField(max_length=100, default=u"")
     mobile = models.CharField(max_length=11, null=True, blank=True)
-    portrait = models.ImageField(upload_to="image/%Y/%m",default=u"image/default.png", max_length=100)
+    portrait = models.ImageField(upload_to="image/%Y/%m", default=u"image/default.png", max_length=100)
 
     class Meta:
         verbose_name = "用户信息"
@@ -17,3 +19,16 @@ class UserProfile(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class CourseComments(models.Model):
+    """课程评论"""
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, verbose_name=u"用户")
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name=u"课程")
+    comments = models.CharField(max_length=200, verbose_name=u"评论")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
+
+    class Meta:
+        verbose_name = u"课程评论"
+        verbose_name_plural = verbose_name
+
